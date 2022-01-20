f14ce6a95c68ef12868fd6d3f779ff3986d0ec86
Johnatan Barbosa <barbosajohnatan99@gmail.com>
2022-01-19 22:52:24 -0300

    Adicionando arquivos index, style e javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrar(){
+  let mostrarRedes = document.querySelector('.sociais')
+  let rotate = document.querySelector('.outline')
+  mostrarRedes.classList.toggle('active')
+  rotate.classList.toggle('active')
+}
