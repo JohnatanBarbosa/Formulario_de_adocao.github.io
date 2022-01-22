e4f18f36df5ec11afedb6a675d6860e4c23ceff3
Johnatan Barbosa <barbosajohnatan99@gmail.com>
2022-01-22 01:20:28 -0300

    Alteração na parte responsiva

--- script.js
+++ script.js
@@ -7,9 +7,6 @@ function mostrar(){
 function trocarAnimal(filename, animalname){
   document.querySelector('.imagem').setAttribute('src', 'assets/' + filename)
   document.querySelector('.imagem').setAttribute('data-animal' + animalname)
-  if(filename == 'Cachorro'){
-    
-  }
 }
 function trocarPorte(filename, animalporte){
   document.querySelector('.porte-animal').setAttribute('src', 'assets/' + filename)
