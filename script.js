0ff525d7963c5d1c9e0607da6c55c17f70b2600f
Johnatan Barbosa <barbosajohnatan99@gmail.com>
2022-01-24 19:51:42 -0300

    Atualização de código JavaScript

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+// Menu de redes sociais 
 function mostrar(){
   let mostrarRedes = document.querySelector('.sociais')
   let rotate = document.querySelector('.outline')
   mostrarRedes.classList.toggle('active')
   rotate.classList.toggle('active')
 }
-function trocarAnimal(filename, animalname){
-  document.querySelector('.imagem').setAttribute('src', 'assets/' + filename)
-  document.querySelector('.imagem').setAttribute('data-animal' + animalname)
+
+// Mostrar foto do cachorro ou gato
+
+function cachorro(){
+  let cachorro = document.querySelector('.animal-image')
+  cachorro.classList.add('cachorro')
+  cachorro.classList.remove('gato')
 }
-function trocarPorte(filename, animalporte){
-  document.querySelector('.porte-animal').setAttribute('src', 'assets/' + filename)
-  document.querySelector('porte-animal').setAttribute('data-porte' + animalporte)
+
+function gato(){
+  let gato = document.querySelector('.animal-image')
+  gato.classList.add('gato')
+  gato.classList.remove('cachorro')
 }
