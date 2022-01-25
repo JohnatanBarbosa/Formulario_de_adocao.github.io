caece397defb4b61fd193bcc9225521eb69c8328
Johnatan Barbosa <barbosajohnatan99@gmail.com>
2022-01-24 22:58:54 -0300

    Mudanças no javascript

--- script.js
+++ script.js
@@ -7,15 +7,45 @@ function mostrar(){
 }
 
 // Mostrar foto do cachorro ou gato
+// Mostrar área para escolher o tamanho do animal
 
 function cachorro(){
   let cachorro = document.querySelector('.animal-image')
+  let mostrarCachorro = document.querySelector('.tamanho-cachorro')
+  let tirarGato = document.querySelector('.tamanho-gato')
+  mostrarCachorro.classList.add('box')
+  tirarGato.classList.remove('box-2')
   cachorro.classList.add('cachorro')
   cachorro.classList.remove('gato')
 }
 
 function gato(){
   let gato = document.querySelector('.animal-image')
+  let mostrarGato = document.querySelector('.tamanho-gato')
+  let tirarCachorro = document.querySelector('.tamanho-cachorro')
+  mostrarGato.classList.add('box-2')
+  tirarCachorro.classList.remove('box')
   gato.classList.add('gato')
   gato.classList.remove('cachorro')
+}
+
+// Trocar tamanho do cachorro
+
+function trocarCachorro(filename, tamanhocachorro){
+  document.querySelector('.tamanhoCachorro').setAttribute('src', 'assets/' + filename)
+  document.querySelector('.tamanhoCachorro').setAttribute('data-cachorro', tamanhocachorro)
+}
+
+// Trocar tamanho do gato
+function trocarGato(filename, tamanhoGato){
+  document.querySelector('.tamanhoGato').setAttribute('src', 'assets/' + filename)
+  document.querySelector('.tamanhoGato').setAttribute('data-gato', tamanhoGato)
+}
+
+// Botão enviar
+function enviar(){
+  let apagar = document.querySelector('.form-box')
+  let mensagem = document.querySelector('.mensagem')
+  apagar.classList.add('clicado')
+  mensagem.classList.add('clicado')
 }
