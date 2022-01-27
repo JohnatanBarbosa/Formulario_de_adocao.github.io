08f365fab10b1cc06369728c1432d0d935a418bf
Johnatan Barbosa <barbosajohnatan99@gmail.com>
2022-01-26 22:11:21 -0300

    Atualização

--- mobile/script.js
+++ mobile/script.js
@@ -3,6 +3,8 @@ function ativarMenu(){
   let menu = document.querySelector('.box-menu')
   let mostrarRedes = document.querySelector('.sociais')
   let rotate = document.querySelector('.outline')
+  let opacity = document.querySelector('ul li')
+  opacity.classList.toggle('active')
   rotate.classList.remove('active')
   mostrarRedes.classList.remove('active')
   menu.classList.toggle('active')
